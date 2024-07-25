c8ab6415031bd8c833b433727f3d2b7719e8552e
Ankesh-287 <ankeshpatil287@gmail.com>
2024-07-25 17:36:25 +0530

    modify the project file

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,43 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/Project.css'
 import Heading from '../components/Heading'
 import { NavLink } from 'react-router-dom'
+import project from "../data/data.json"
 
 function Project() {
-    return (
-        <>
-            <div className="project-section">
-                <Heading title="My Projects" />
-                <NavLink to='/project'><div className="see">See more...</div></NavLink>
-                <div className="project-page">
-                    <NavLink to="https://my-portfolio-ankesh-287s-projects.vercel.app/">
-                        <div className="project-card">
-                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/portfolio.png?raw=true" alt="react" />
-                            <p className="lang-text">Portfolio</p>
-                        </div>
-                    </NavLink>
-                    <NavLink to="https://ankesh-287.github.io/Calculator.github.io/">
-                        <div className="project-card">
-                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/calculator.png?raw=true" alt="react" />
-                            <p className="lang-text">Calculator</p>
-                        </div>
-                    </NavLink>
-                    <NavLink to="https://ankesh-287.github.io/ToDoList/">
-                        <div className="project-card">
-                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/todo.png?raw=true" alt="react" />
-                            <p className="lang-text">Todo</p>
-                        </div>
-                    </NavLink>
-                    <NavLink to="https://bg-changer-peach-eight.vercel.app/">
-                        <div className="project-card">
-                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/background.png?raw=true" alt="react" />
-                            <p className="lang-text">Background Changer</p>
-                        </div>
-                    </NavLink>
-                </div>
-            </div>
-        </>
-    )
+    
+  const [limit, setLimit] = useState(4)
+
+  const handleData = () => {
+    if (limit === 4) {
+      setLimit(project.length) 
+    } else {
+      setLimit(4)
+    }
+  }
+
+  return (
+    <>
+      <div className="project-section">
+        <Heading title="My Projects" />\
+        <NavLink to='/project'>
+          <div className="see" onClick={handleData}>
+            {limit === 4 ? 'See more...' : 'See less...'}
+          </div>
+        </NavLink>
+
+        <div className="project-page">
+          {project.slice(0, limit).map((data) => (
+            <NavLink to={data.link} key={data.index}>
+              <div className="project-card">
+                <img src={data.image} alt={data.name} />
+                <p className="lang-text">{data.name}</p>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Project
