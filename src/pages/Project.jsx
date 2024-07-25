3238e7b7bdc8c09560fbf46ccba55417671077ff
Ankesh-287 <ankeshpatil287@gmail.com>
2024-07-25 11:35:22 +0530

    add project file

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,1 +1,41 @@
+import React from 'react'
+import '../style/Project.css'
+import Heading from '../components/Heading'
 
+function Project() {
+    return (
+        <>
+            <div className="project-section">
+                <Heading title="My Projects" />
+                <div className="project-page">
+                    <a href="">
+                        <div className="project-card">
+                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/portfolio.png?raw=true" alt="react" />
+                            <p className="lang-text">Portfolio</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="project-card">
+                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/calculator.png?raw=true" alt="react" />
+                            <p className="lang-text">Calculator</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="project-card">
+                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/todo.png?raw=true" alt="react" />
+                            <p className="lang-text">Todo</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="project-card">
+                            <img src="https://github.com/Ankesh-287/my-portfolio/blob/main/public/currency.png?raw=true" alt="react" />
+                            <p className="lang-text">Currency Converter</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Project
